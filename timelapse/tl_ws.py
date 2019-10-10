bffb2ea1bababd15378f9aa988b6a61ff6ca2950
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2019-10-10 07:12:06 +0100

    To Python 3, add support for favicon

--- timelapse/tl_ws.py
+++ timelapse/tl_ws.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """
 Timelapse Web Server
@@ -10,94 +10,17 @@ import re
 import time
 import sys
 import os
+import socket
 import traceback
 import subprocess
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
 
 
 """
 TODO
 - Report how long ago the file was taken
 - Report reasons for missing picture: no dir, empty dir, etc
-
-FIXME
-192.168.1.106 - - [06/Oct/2019 14:25:23] "GET /2019/tl_1006142513.jpg HTTP/1.1" 200 -
-Caught exception!
-<class 'socket.error'>
-Traceback (most recent call last):
-  File "/home/pi/code_pi/timelapse/tl_ws.py", line 159, in do_GET
-    return SimpleHTTPRequestHandler.do_GET(self)
-  File "/usr/lib/python2.7/SimpleHTTPServer.py", line 48, in do_GET
-    self.copyfile(f, self.wfile)
-  File "/usr/lib/python2.7/SimpleHTTPServer.py", line 192, in copyfile
-    shutil.copyfileobj(source, outputfile)
-  File "/usr/lib/python2.7/shutil.py", line 66, in copyfileobj
-    fdst.write(buf)
-  File "/usr/lib/python2.7/socket.py", line 328, in write
-    self.flush()
-  File "/usr/lib/python2.7/socket.py", line 307, in flush
-    self._sock.sendall(view[write_offset:write_offset+buffer_size])
-error: [Errno 113] No route to host
-----------------------------------------
-Exception happened during processing of request from ('192.168.1.106', 38184)
-Traceback (most recent call last):
-  File "/usr/lib/python2.7/SocketServer.py", line 293, in _handle_request_noblock
-    self.process_request(request, client_address)
-  File "/usr/lib/python2.7/SocketServer.py", line 321, in process_request
-    self.finish_request(request, client_address)
-  File "/usr/lib/python2.7/SocketServer.py", line 334, in finish_request
-    self.RequestHandlerClass(request, client_address, self)
-  File "/usr/lib/python2.7/SocketServer.py", line 657, in __init__
-    self.finish()
-  File "/usr/lib/python2.7/SocketServer.py", line 716, in finish
-    self.wfile.close()
-  File "/usr/lib/python2.7/socket.py", line 283, in close
-    self.flush()
-  File "/usr/lib/python2.7/socket.py", line 307, in flush
-    self._sock.sendall(view[write_offset:write_offset+buffer_size])
-error: [Errno 32] Broken pipe
-----------------------------------------
-
-After CTRL-C it logged below and resumed working:
-
-^C----------------------------------------
-Exception happened during processing of request from ('192.168.1.106', 38188)
-Traceback (most recent call last):
-  File "/usr/lib/python2.7/SocketServer.py", line 293, in _handle_request_noblock
-    self.process_request(request, client_address)
-  File "/usr/lib/python2.7/SocketServer.py", line 321, in process_request
-    self.finish_request(request, client_address)
-  File "/usr/lib/python2.7/SocketServer.py", line 334, in finish_request
-    self.RequestHandlerClass(request, client_address, self)
-  File "/usr/lib/python2.7/SocketServer.py", line 655, in __init__
-    self.handle()
-  File "/usr/lib/python2.7/BaseHTTPServer.py", line 340, in handle
-    self.handle_one_request()
-  File "/usr/lib/python2.7/BaseHTTPServer.py", line 310, in handle_one_request
-    self.raw_requestline = self.rfile.readline(65537)
-  File "/usr/lib/python2.7/socket.py", line 480, in readline
-    data = self._sock.recv(self._rbufsize)
-KeyboardInterrupt
-----------------------------------------
-192.168.1.106 - - [06/Oct/2019 16:16:10] "GET /tl HTTP/1.1" 200 -
-192.168.1.106 - - [06/Oct/2019 16:16:11] "GET /tl HTTP/1.1" 200 -
-192.168.1.106 - - [06/Oct/2019 16:16:12] "GET /tl HTTP/1.1" 200 -
-192.168.1.106 - - [06/Oct/2019 16:16:13] "GET /tl HTTP/1.1" 200 -
-192.168.1.106 - - [06/Oct/2019 16:16:13] "GET /tl HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:17:39] "GET /tl HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:17:39] "GET /2019/tl_1006161733.jpg HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:17:45] "GET /tl HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:17:46] "GET /2019/tl_1006161743.jpg HTTP/1.1" 200 -
-
-
-Another CTRL-C got it killed as expected:
-
-192.168.1.101 - - [06/Oct/2019 16:19:15] "GET /2019/tl_1006161913.jpg HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:19:25] "GET /tl HTTP/1.1" 200 -
-192.168.1.101 - - [06/Oct/2019 16:19:26] "GET /2019/tl_1006161923.jpg HTTP/1.1" 200 -
-^C^C received, shutting down server
-
 """
 
 
@@ -105,6 +28,7 @@ Another CTRL-C got it killed as expected:
 # Configuration
 #
 PORT = 8007
+FAVICON = '/home/pi/code_pi/timelapse/favicon.ico'
 DS18B20_OUTPUT_FILE = '/sys/bus/w1/devices/28-0000045bf342/w1_slave'
 
 
@@ -112,7 +36,7 @@ def run_cmd(cmd):
     """ Utility that gets the first line from a cmd launched on a shell """
     pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     output = pipe.communicate()[0]
-    return output
+    return str(output)
 
 
 regex = re.compile(r't=(\d+)')
@@ -135,7 +59,7 @@ def get_temperature_DS18B20():
 def get_temperature():
 
     # Uncomment if no temperature sensor attached
-    #return 'N/A'
+    # return 'N/A'
     return run_cmd('sudo /home/pi/code_pi/HTU21/HTU21D.py')
 
 
@@ -160,15 +84,15 @@ body_template = """<HTML>
 def get_picture_filename():
     # Filter for directory names starting with '201'
     try:
-        last_dir_name = max(filter(lambda dir: dir.find('2019') != -1, os.listdir('.')))
-        if last_dir_name == None:
+        last_dir_name = max([dir for dir in os.listdir('.') if dir.find('2019') != -1])
+        if last_dir_name is None:
             print('WARNING: last_dir_name == None')
             return None
         if len(last_dir_name) == 0:
             print('WARNING: len(last_dir_name) == 0')
             return None
-    except:
-        print('WARNING: exception thrown - no dir starting with 201')
+    except Exception:
+        print('WARNING: exception thrown - no dir starting with 201', sys.exc_info()[0])
         return None
 
     last_dir_path = os.path.join('.', last_dir_name)
@@ -204,17 +128,17 @@ def make_body():
 
     image = get_picture_filename()
     if image is None:
-        #image = os.path.join('.', 'out_of_hours_picture.jpg')
+        # image = os.path.join('.', 'out_of_hours_picture.jpg')
         image = 'out_of_hours_picture.jpg'
 
     # print 'Image path:', image
     if image is not None:
         last_modified_date = time.ctime(os.path.getmtime(image))
-        body = body.replace('XXX_IMAGE_XXX', image);
+        body = body.replace('XXX_IMAGE_XXX', image)
 
         body = body.replace('XXX_INFO_XXX', image + ' --- ' + str(get_temperature()) +
                             '--- ' + last_modified_date)
-        #body = body.replace('XXX_INFO_XXX', 'St. George Wharf *** ' + last_modified_date)
+        # body = body.replace('XXX_INFO_XXX', 'St. George Wharf *** ' + last_modified_date)
 
     return body
 
@@ -229,40 +153,51 @@ class MyHandler(SimpleHTTPRequestHandler, object):
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
 
-                self.wfile.write('Testing. Site under construction.')
-                self.wfile.close()
+                self.wfile.write(b'Testing. Site under construction.')
                 return
 
-            if self.path != "/tl":
+            elif self.path == "/favicon.ico":
+                with open(FAVICON, 'rb') as f:
+                    favicon = f.read()
+
+                self.send_response(200)
+                self.end_headers()
+
+                self.wfile.write(favicon)
+                return
+
+            elif self.path != "/tl":
                 return SimpleHTTPRequestHandler.do_GET(self)
 
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            else:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
 
-            self.wfile.write(make_body())
+                self.wfile.write(str.encode(make_body()))
 
-            self.wfile.close()
+        except socket.error as e:
+            print('do_GET(): Caught socket.error exception:', e)
+            traceback.print_exc()
 
-        except:
-            print('Caught exception!')
-            print(sys.exc_info()[0])
+        # Usually is <class 'socket.error'>, error: [Errno 113] No route to host
+        except Exception:
+            print('do_GET(): Caught exception:', sys.exc_info()[0])
             traceback.print_exc()
 
 
 try:
-    TIMELAPSE_CODE_PATH = '/home/pi/code_pi/timelapse'
-    os.chdir(TIMELAPSE_CODE_PATH)
-
     timelapse_dir = subprocess.check_output(['readlink', 'timelapse_dir']).rstrip()
-    print('Serving from ' + timelapse_dir)
+    print('Image directory:', timelapse_dir.decode())
 
     os.chdir(timelapse_dir)
 
     server = HTTPServer(('', PORT), MyHandler)
-    print('Started HTTP server on port ' + str(PORT))
+    print('Started the webcam HTTP server on port', PORT)
     server.serve_forever()
 
+    print('CRASHED OUT OF THE SERVER LOOP')
+
 except KeyboardInterrupt:
-    print("^C received, shutting down server")
+    print("\n^C received, shutting down server")
     server.socket.close()
