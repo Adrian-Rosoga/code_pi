82758056f21ca7d91d4cb7f911a6c28520927c3c
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2019-11-01 14:01:05 +0000

    Refactoring

--- timelapse/tl_ws.py
+++ timelapse/tl_ws.py
@@ -33,7 +33,30 @@ TODO
 #
 PORT = 8007
 FAVICON = '/home/pi/code_pi/timelapse/favicon.ico'
+OUT_OF_HOURS = '/home/pi/code_pi/timelapse/out_of_hours.jpg'
 DS18B20_OUTPUT_FILE = '/sys/bus/w1/devices/28-0000045bf342/w1_slave'
+
+BODY_TEMPLATE = """<HTML>
+<HEAD>
+<TITLE>{title}</TITLE>
+<META HTTP-EQUIV=PRAGMA CONTENT=NO-CACHE>
+<META HTTP-EQUIV=EXPIRES CONTENT=-1>
+<META HTTP-EQUIV=REFRESH CONTENT={refresh}>
+</HEAD>
+<BODY BGCOLOR="BLACK" BACKGROUND_X="grill.jpg">
+<H2 ALIGN="CENTER">
+<B><FONT COLOR="YELLOW">{info}</FONT></B>
+</H2>
+<P ALIGN="CENTER">
+<IMG SRC="{image}" STYLE="HEIGHT: 100%" ALT="NO IMAGE!" WIDTH_X="420" HEIGHT_X="420">
+</P>
+</BODY>
+</HTML>"""
+
+
+class Registry(object):
+    title = "TITLE"
+    refresh = 10
 
 
 def run_cmd(cmd):
@@ -68,42 +91,24 @@ def get_temperature():
         return "{0:0.1f}&deg;C - {1:0.1f}%H".format(temperature, humidity)
 
 
-body_template = """<HTML>
-<HEAD>
-<TITLE>Team GBLM</TITLE>
-<META HTTP-EQUIV=PRAGMA CONTENT=NO-CACHE>
-<META HTTP-EQUIV=EXPIRES CONTENT=-1>
-<META HTTP-EQUIV=REFRESH CONTENT=10>
-</HEAD>
-<BODY BGCOLOR="BLACK" BACKGROUND_X="grill.jpg">
-<H2 ALIGN="CENTER">
-<B><FONT COLOR="YELLOW">XXX_INFO_XXX</FONT></B>
-</H2>
-<P ALIGN="CENTER">
-<IMG SRC="XXX_IMAGE_XXX" STYLE="HEIGHT: 100%" ALT="NO IMAGE!" WIDTH_X="420" HEIGHT_X="420">
-</P>
-</BODY>
-</HTML>"""
-
-
 def get_picture_filename():
     # Filter for directory names starting with '201'
     try:
         last_dir_name = max([directory for directory in os.listdir('.') if directory.find('2019') != -1])
         if last_dir_name is None:
-            print('WARNING: last_dir_name == None')
+            logging.info('WARNING: last_dir_name == None')
             return None
         if len(last_dir_name) == 0:
-            print('WARNING: len(last_dir_name) == 0')
+            logging.info('WARNING: len(last_dir_name) == 0')
             return None
     except Exception:
-        print('WARNING: exception thrown - no dir starting with 201', sys.exc_info()[0])
+        logging.info('WARNING: exception thrown - no dir starting with 201', sys.exc_info()[0])
         return None
 
     last_dir_path = os.path.join('.', last_dir_name)
     if not os.path.isdir(last_dir_path):
-        print('WARNING: not os.path.isdir(last_dir_path)')
-        print('last_dir_path = ' + last_dir_path)
+        logging.info('WARNING: not os.path.isdir(last_dir_path)')
+        logging.info('last_dir_path = ' + last_dir_path)
         return None
 
     files = os.listdir(last_dir_path)
@@ -111,7 +116,7 @@ def get_picture_filename():
     files.sort(key=lambda x: x)
 
     if len(files) == 0:
-        print('WARNING: len(files) == 0')
+        logging.info('WARNING: len(files) == 0')
         return None
 
     # Last picture is the one we are looking for
@@ -122,33 +127,25 @@ def get_picture_filename():
         return os.path.join(last_dir_path, last_picture)
     else:
         if len(files) < 2:
-            print('WARNING: len(files) < 2')
+            logging.info('WARNING: len(files) < 2')
             return None
         last_picture = files[-2]
         return os.path.join(last_dir_path, last_picture)
 
 
 def make_body():
-    body = body_template
-
     image = get_picture_filename()
     if image is None:
-        # image = os.path.join('.', 'out_of_hours_picture.jpg')
-        image = 'out_of_hours_picture.jpg'
-
-    # print 'Image path:', image
-    if image is not None:
+        image = 'out_of_hours.jpg'
+        info = str(get_temperature())
+    else:
         last_modified_date = time.ctime(os.path.getmtime(image))
-        body = body.replace('XXX_IMAGE_XXX', image)
+        info = last_modified_date + ' --- ' + str(get_temperature())
 
-        body = body.replace('XXX_INFO_XXX', image + ' --- ' + str(get_temperature()) +
-                            ' --- ' + last_modified_date)
-        # body = body.replace('XXX_INFO_XXX', 'St. George Wharf *** ' + last_modified_date)
-
-    return body
+    return BODY_TEMPLATE.format(image=image, info=info, title=Registry.title, refresh=Registry.refresh)
 
 
-class MyHandler(SimpleHTTPRequestHandler, object):
+class WebcamHandler(SimpleHTTPRequestHandler, object):
 
     def do_GET(self):
 
@@ -162,10 +159,18 @@ class MyHandler(SimpleHTTPRequestHandler, object):
 
             elif self.path == "/favicon.ico":
                 with open(FAVICON, 'rb') as f:
-                    favicon = f.read()
+                    content = f.read()
                     self.send_response(200)
                     self.end_headers()
-                    self.wfile.write(favicon)
+                    self.wfile.write(content)
+                    return
+
+            elif self.path == "/out_of_hours.jpg":
+                with open(OUT_OF_HOURS, 'rb') as f:
+                    content = f.read()
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(content)
                     return
 
             elif self.path != "/tl":
@@ -178,12 +183,12 @@ class MyHandler(SimpleHTTPRequestHandler, object):
                 self.wfile.write(str.encode(make_body()))
 
         except socket.error as e:
-            print('do_GET(): Caught socket.error exception:', e)
+            logging.info('do_GET(): Caught socket.error exception:', e)
             traceback.print_exc()
 
         # Usually is <class 'socket.error'>, error: [Errno 113] No route to host
         except Exception:
-            print('do_GET(): Caught exception:', sys.exc_info()[0])
+            logging.info('do_GET(): Caught exception:', sys.exc_info()[0])
             traceback.print_exc()
 
 
@@ -192,12 +197,22 @@ try:
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.INFO)
 
+    parser = argparse.ArgumentParser(description='WebServer')
+    parser.add_argument('--title', help='set title')
+    parser.add_argument('--refresh', help='automatic refresh in seconds')
+    args = parser.parse_args()
+
+    if args.title:
+        Registry.title = args.title
+    if args.refresh:
+        Registry.refresh = args.refresh
+
     timelapse_dir = subprocess.check_output(['readlink', 'timelapse_dir']).rstrip()
     logging.info('Image directory: %s', timelapse_dir.decode())
 
     os.chdir(timelapse_dir)
 
-    server = HTTPServer(('', PORT), MyHandler)
+    server = HTTPServer(('', PORT), WebcamHandler)
     logging.info('Started the webcam HTTP server on port %s', PORT)
     server.serve_forever()
 
