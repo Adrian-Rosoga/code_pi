22fbe6735bffed704c0ba12ad1ed6613abbaf69a
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2019-12-29 17:17:16 +0000

    Retry if returned value is none decorator and minor fixes

--- utilpy/util.py
+++ utilpy/util.py
@@ -1,8 +1,40 @@
-#!/usr/bin/env python3
+import functools
+import time
+import subprocess
+
+
+def timeit(func):
+    """Basic timing decorator"""
+    @functools.wraps(func)
+    def timed(*args, **kw):
+        ts = time.time()
+        result = func(*args, **kw)
+        te = time.time()
+        print(f'{func.__name__}: {(te - ts) * 1000:.2f} ms')
+        return result
+    return timed
+
+
+def retry_if_return_is_none(times=1, sleep_secs=1):
+    """Decorator - retry a configurable number of times if the return value is None"""
+    def wrapper(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kw):
+            count = times
+            while count > 0:
+                result = func(*args, **kw)
+                if result is not None:
+                    return result
+                else:
+                    time.sleep(sleep_secs)
+                    count -= 1
+                    continue
+        return wrapped
+    return wrapper
 
 
 def run_cmd(cmd):
-    """ Utility that gets the first line from a cmd launched on a shell """
+    """Return the first output line from a cmd launched on a shell"""
     pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     output = pipe.communicate()[0]
-    return str(output)
+    return output.decode().split('\n')[0]
