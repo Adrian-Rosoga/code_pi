8a5a9014952dae45f6e511b1cd42cc7cbac3b215
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2019-12-30 17:24:27 +0000

    PiWebServer files

--- PiWebServer/app/static/scripts.js
+++ PiWebServer/app/static/scripts.js
@@ -1,12 +1,12 @@
-// a couple of different backgrounds to style the shop
+// A couple of different backgrounds
 var background1 = 'black';
 var background2 = 'firebrick';
 
-// this lets us toggle the background state
+// Toggle the background state
 var color = true;
 
 // every 1 second, switch the background color, alternating between the two styles
 setInterval(function () {
   document.body.style.backgroundColor = (color ? background1 : background2)
   color = !color;
-}, 1000);
+}, 10000);
