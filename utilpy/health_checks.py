01b1a911152c340f9903d08ca254733314e91246
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2020-04-29 22:03:48 +0100

    Rafactor using namedtuple

--- utilpy/health_checks.py
+++ utilpy/health_checks.py
@@ -10,6 +10,7 @@ import argparse
 import smtplib
 import ssl
 import threading
+from collections import namedtuple
 
 
 #
@@ -30,32 +31,38 @@ TIMEOUT_SECS = 10
 #URLs = ['http://tides-env.m5xmmhjzmw.eu-west-2.elasticbeanstalk.comX']
 
 
+URLData = namedtuple('URLData', 'code exception_name data_length time_ms')
+
+
 class URLsStatus:
 
     def __init__(self):
         self.status = dict()
         self._lock = threading.Lock()
 
-    def update(self, url, code, data_length, time_ms):
+    def update(self, url, code, exception_name, data_length, time_ms):
 
         with self._lock:
-            self.status[url] = [code, data_length, time_ms]
+            self.status[url] = URLData(code, exception_name, data_length, time_ms)
 
     def values(self):
 
         for url, data in self.status.items():
-            yield url, data[0], data[1], data[2]
+            yield url, data
 
 
 def check_web_site(url: str, urls_status):
+
+    exception_name = None
 
     ts = time.time()
 
     try:
         page = requests.get(url, timeout=TIMEOUT_SECS)
         code = page.status_code
-    except requests.exceptions.RequestException as exception:
-        code = 777
+    except requests.exceptions.RequestException as ex:
+        code = None
+        exception_name = ex.__class__.__name__
 
     te = time.time()
 
@@ -63,7 +70,7 @@ def check_web_site(url: str, urls_status):
     
     data_length = len(page.text) if code == requests.codes.ok else 0
 
-    urls_status.update(url, code, data_length, time_ms)
+    urls_status.update(url, code, exception_name, data_length, time_ms)
 
     return code, data_length
 
@@ -91,6 +98,8 @@ def main():
 
     url_status = URLsStatus()
 
+    ts = time.time()
+
     threads = list()
     for url in URLs:
 
@@ -102,18 +111,30 @@ def main():
     for thread in threads:
         thread.join()
 
+    te = time.time()
+
+    time_ms = (te - ts) * 1000
+
     status_codes = []
     messages = []
-    for url, status_code, data_length, time_ms in url_status.values():
+    for url, url_data in url_status.values():
+
+        status_code, exception_name, data_length, time_ms = url_data
 
         if status_code != requests.codes.ok:
             status_codes.append(False)
-            print(f'{url} is DOWN! Status code {status_code}! Took {time_ms:.2f} ms')
-            messages.append(f'{url} is DOWN! Status code {status_code}! Took {time_ms:.2f} ms')
+            msg = f'{url} DOWN! Code {status_code}. Exception {exception_name}. Took {time_ms:.2f} ms'
         else:
             status_codes.append(True)
-            print(f'{url} is UP! Status code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms')
-            messages.append(f'{url} is UP! Status code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms')
+            msg = f'{url} UP! Code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms'
+
+        print(msg)
+        messages.append(msg)
+
+    messages.append("")
+    msg = f"All checks took {time_ms:.2f} ms"
+    print(msg)
+    messages.append(msg)
 
     if all(status_codes):
         subject = f'WebChecks OK: All {len(status_codes)} sites are UP!'
