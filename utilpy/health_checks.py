1ce0bd36df1c3355eb6e41f7f4446794071bab54
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2020-05-07 22:32:31 +0100

    Send email after a configurable number of successes

--- utilpy/health_checks.py
+++ utilpy/health_checks.py
@@ -4,12 +4,12 @@
 import os
 import sys
 import time
-import datetime
 import requests
-import argparse
 import smtplib
 import ssl
 import threading
+import pickle
+import datetime
 from collections import namedtuple
 from http import HTTPStatus
 
@@ -27,10 +27,8 @@ URLs = ['http://tides-env.m5xmmhjzmw.eu-west-2.elasticbeanstalk.com',
         'https://d2eafeb7.eu.ngrok.io/tl'
         ]
 
-TIMEOUT_SECS = 10
-
-#URLs = ['http://tides-env.m5xmmhjzmw.eu-west-2.elasticbeanstalk.comX']
-
+TIMEOUT_SECS = 30
+STATUS_STORE_FILE = "/home/adi/code_local/health_check_status.dat"
 
 URLData = namedtuple('URLData', 'url code ex_type ex_value data_length time_ms')
 
@@ -42,6 +40,29 @@ def HTTP_info_from_code(code: int):
             return status.name, status.phrase, status.description
 
     return (None,) * 3
+
+
+class ManagedTimerSeconds:
+
+    def __init__(self, duration_out):
+        self.time_start = None
+        self.duration_out = duration_out
+
+    def __enter__(self):
+        self.time_start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.duration_out *= 0
+        self.duration_out.append((time.time() - self.time_start))
+
+
+class StatusStore:
+
+    def __init__(self):
+        self.messages = []
+
+    def add(self, message):
+        self.messages.append(message)
 
 
 def TimeFn(elem):
@@ -67,7 +88,7 @@ class URLsStatus:
     def values_sorted_by_time(self):
 
         for url_status in sorted(self.status, key=TimeFn):
-            yield url_status        
+            yield url_status
 
 
 def check_web_site(url: str, urls_status):
@@ -75,21 +96,20 @@ def check_web_site(url: str, urls_status):
     ex_type = None
     ex_value = None
 
-    ts = time.time()
+    elapsed_secs = []
+    with ManagedTimerSeconds(elapsed_secs):
 
-    try:
-        page = requests.get(url, timeout=TIMEOUT_SECS)
-        code = page.status_code
-    except requests.exceptions.RequestException as ex:
-        code = None
-        ex_type = ex.__class__.__name__
-        ex_type, ex_value, _ = sys.exc_info()
+        try:
+            page = requests.get(url, timeout=TIMEOUT_SECS)
+            code = page.status_code
+        except requests.exceptions.RequestException as ex:
+            code = None
+            ex_type = ex.__class__.__name__
+            ex_type, ex_value, _ = sys.exc_info()
 
-    te = time.time()
+    time_ms = elapsed_secs[0] * 1000
 
-    time_ms = (te - ts) * 1000
-    
-    data_length = len(page.text) if code == requests.codes.ok else 0
+    data_length = len(page.text) if code == requests.codes.OK else 0
 
     urls_status.update(url, code, ex_type, ex_value, data_length, time_ms)
 
@@ -113,73 +133,117 @@ def send_email(subject, message):
         server.sendmail(sender_email, receiver_email, message)
 
 
+def build_message(values, messages=None, status_codes=None):
+
+    for status in values:
+
+        url, status_code, ex_type, ex_value, data_length, time_ms = status
+
+        if status_code != requests.codes.ok:
+            if status_codes is not None:
+                status_codes.append(False)
+            HTTPName, HTTPPhrase, HTTPDescription = HTTP_info_from_code(status_code)
+            msg = f'{url} DOWN! Code {status_code}. Name \'{HTTPName}\'. Phrase \'{HTTPPhrase}\'. Description \'{HTTPDescription}\'. Ex_type {ex_type}. Ex_value \'{ex_value}\'. Took {time_ms:.2f} ms'
+        else:
+            if status_codes is not None:
+                status_codes.append(True)
+            msg = f'{url} UP! Code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms'
+
+        print(msg)
+        if messages is not None:
+            messages.append(msg)
+
+
 def main():
+
+    messages = ["\nHealth chech performed at " + str(datetime.datetime.now())]
 
     url_status = URLsStatus()
 
-    ts = time.time()
+    elapsed_secs = []
+    with ManagedTimerSeconds(elapsed_secs):
 
-    threads = list()
-    for url in URLs:
+        threads = []
+        for url in URLs:
 
-        print(f'Checking {url}...')
-        thread = threading.Thread(target=check_web_site, args=(url, url_status))
-        threads.append(thread)
-        thread.start()
+            print(f'Checking {url}...')
+            thread = threading.Thread(target=check_web_site, args=(url, url_status))
+            threads.append(thread)
+            thread.start()
 
-    for thread in threads:
-        thread.join()
+        for thread in threads:
+            thread.join()
 
-    te = time.time()
-
-    time_ms = (te - ts) * 1000
+    time_ms = elapsed_secs[0] * 1000
 
     status_codes = []
-    messages = ["\n"]
 
-    for status in url_status.values():
-
-        url, status_code, ex_type, ex_value, data_length, time_ms = status
-
-        if status_code != requests.codes.ok:
-            status_codes.append(False)
-            HTTPName, HTTPPhrase, HTTPDescription = HTTP_info_from_code(status_code)
-            msg = f'{url} DOWN! Code {status_code}. Name \'{HTTPName}\'. Phrase \'{HTTPPhrase}\'. Description \'{HTTPDescription}\'. Ex_type {ex_type}. Ex_value \'{ex_value}\'. Took {time_ms:.2f} ms'
-        else:
-            status_codes.append(True)
-            msg = f'{url} UP! Code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms'
-
-        print(msg)
-        messages.append(msg)
-
+    build_message(url_status.values(), messages=None, status_codes=status_codes)
+    
     messages.append("\n\nOrdered by time\n")
 
     # Almost the same but sort by time
-    for status in url_status.values_sorted_by_time():
-
-        url, status_code, ex_type, ex_value, data_length, time_ms = status
-
-        if status_code != requests.codes.ok:
-            HTTPName, HTTPPhrase, HTTPDescription = HTTP_info_from_code(status_code)
-            msg = f'{url} DOWN! Code {status_code}. Name \'{HTTPName}\'. Phrase \'{HTTPPhrase}\'. Description \'{HTTPDescription}\'. Ex_type {ex_type}. Ex_value \'{ex_value}\'. Took {time_ms:.2f} ms'
-        else:
-            msg = f'{url} UP! Code {status_code}. Data length = {data_length}. Took {time_ms:.2f} ms'
-
-        print(msg)
-        messages.append(msg)
+    build_message(url_status.values_sorted_by_time(), messages, status_codes=None)
 
     messages.append("")
     msg = f"All checks took {time_ms:.2f} ms"
     print(msg)
     messages.append(msg)
 
-    if all(status_codes):
-        subject = f'WebChecks OK: All {len(status_codes)} sites are UP!'
-    else:
-        subject = f'WebChecks FAIL for at least one site!'
+    try:
+        with open(STATUS_STORE_FILE, "rb") as fd:
+            status_store = pickle.load(fd)
+    except:
+        status_store = None
 
-    print(f'Sending email...')
-    send_email(subject, "\n".join(messages))
+    current_email_body = "\n".join(messages)
+
+    if all(status_codes):
+
+        if status_store is None:
+            status_store = StatusStore()
+
+        status_store.add(current_email_body)
+
+        if len(status_store.messages) == 4:
+
+            subject = f'WebChecks OK: All {len(status_codes)} sites are UP!'
+            print(f'Sending email to inform of {len(status_store.messages)} success checks so far...')
+
+            email_body = ""
+
+            for message in status_store.messages:
+                email_body += "\n"
+                email_body += message
+
+            send_email(subject, email_body)
+
+            # Clear the status store
+
+            with open(STATUS_STORE_FILE, "wb") as fd:
+                pickle.dump(StatusStore(), fd)
+
+        else:
+
+            with open(STATUS_STORE_FILE, "wb") as fd:
+                pickle.dump(status_store, fd)
+
+            print(f'Not sending emails, {len(status_store.messages)} ok so far...')
+    else:
+        subject = f'WebChecks FAILED for at least one site!'
+        
+        if status_store is not None:
+
+            email_body = ""
+
+            for message in status_store.messages:
+                email_body += "\n"
+                email_body += message
+
+        email_body += current_email_body
+
+        print(f'Sending email to inform of failure...')
+        send_email(subject, email_body)
 
 
 if __name__ == '__main__':
