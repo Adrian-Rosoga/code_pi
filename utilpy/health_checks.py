ff057174752fc4e13942f70575d3df076a0efe80
Adrian-Rosoga <Adrian-Rosoga@users.noreply.github.com>
2020-05-08 06:49:47 +0100

    Simplify code

--- utilpy/health_checks.py
+++ utilpy/health_checks.py
@@ -29,6 +29,7 @@ URLs = ['http://tides-env.m5xmmhjzmw.eu-west-2.elasticbeanstalk.com',
 
 TIMEOUT_SECS = 30
 STATUS_STORE_FILE = "/home/adi/code_local/health_check_status.dat"
+NUMBER_BACKLOG_CHECKS = 4
 
 URLData = namedtuple('URLData', 'url code ex_type ex_value data_length time_ms')
 
@@ -59,10 +60,10 @@ class ManagedTimerSeconds:
 class StatusStore:
 
     def __init__(self):
-        self.messages = []
+        self.statuses = []
 
-    def add(self, message):
-        self.messages.append(message)
+    def add(self, status):
+        self.statuses.append(status)
 
 
 def TimeFn(elem):
@@ -180,13 +181,12 @@ def main():
 
     build_message(url_status.values(), messages=None, status_codes=status_codes)
     
-    messages.append("\n\nOrdered by time\n")
+    messages.append("\nOrdered by response time:")
 
     # Almost the same but sort by time
     build_message(url_status.values_sorted_by_time(), messages, status_codes=None)
 
-    messages.append("")
-    msg = f"All checks took {time_ms:.2f} ms"
+    msg = f"Running all checks took {time_ms:.2f} ms"
     print(msg)
     messages.append(msg)
 
@@ -194,32 +194,25 @@ def main():
         with open(STATUS_STORE_FILE, "rb") as fd:
             status_store = pickle.load(fd)
     except:
-        status_store = None
+        status_store = StatusStore()
 
     current_email_body = "\n".join(messages)
 
     if all(status_codes):
 
-        if status_store is None:
-            status_store = StatusStore()
-
         status_store.add(current_email_body)
 
-        if len(status_store.messages) == 4:
+        # Send email right away if enough reports in the backlog, else just store the report
+        if len(status_store.statuses) == NUMBER_BACKLOG_CHECKS:
 
             subject = f'WebChecks OK: All {len(status_codes)} sites are UP!'
-            print(f'Sending email to inform of {len(status_store.messages)} success checks so far...')
+            print(f'Sending email to inform of {len(status_store.statuses)} successful checks so far...')
 
-            email_body = ""
-
-            for message in status_store.messages:
-                email_body += "\n"
-                email_body += message
+            email_body = "\n\n======================\n".join(status_store.statuses)
 
             send_email(subject, email_body)
 
             # Clear the status store
-
             with open(STATUS_STORE_FILE, "wb") as fd:
                 pickle.dump(StatusStore(), fd)
 
@@ -228,22 +221,22 @@ def main():
             with open(STATUS_STORE_FILE, "wb") as fd:
                 pickle.dump(status_store, fd)
 
-            print(f'Not sending emails, {len(status_store.messages)} ok so far...')
+            print(f'Not sending email report, {len(status_store.statuses)} checks OK so far...')
+    
     else:
+
         subject = f'WebChecks FAILED for at least one site!'
         
-        if status_store is not None:
+        email_body = "\n\n======================\n".join(status_store.statuses)
 
-            email_body = ""
-
-            for message in status_store.messages:
-                email_body += "\n"
-                email_body += message
-
-        email_body += current_email_body
+        email_body += "\n\n=== FAILED CHECKS ===================\n" + current_email_body
 
         print(f'Sending email to inform of failure...')
         send_email(subject, email_body)
+
+        # Clear the status store
+        with open(STATUS_STORE_FILE, "wb") as fd:
+            pickle.dump(StatusStore(), fd)
 
 
 if __name__ == '__main__':
